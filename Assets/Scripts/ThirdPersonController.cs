1c7854e21339d60d38c6d8bbad9f7a3ae6cfde49
FunsTW <45548858+FunsTW@users.noreply.github.com>
2020-07-25 22:43:03 +0800

    First Update

---
+++ Assets/Scripts/ThirdPersonController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonController : MonoBehaviour
+{
+    [SerializeField] private Transform m_camera = null;
+    [SerializeField] private float m_speed = 1;
+    void Awake() {
+        m_camera = Camera.main.gameObject.transform;
+    }
+    void Update() {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0,Input.GetAxis("Vertical"));
+        movement = movement * m_speed * Time.deltaTime;
+        movement = Quaternion.Euler(0, m_camera.localEulerAngles.y, 0) * movement;
+        transform.position = transform.position + movement;
+
+        Vector3 rotation = transform.eulerAngles;
+        rotation.y = m_camera.eulerAngles.y;
+        transform.eulerAngles = rotation;
+
+        if(transform.position.y < -1) {
+            transform.position = new Vector3(0, 1, 0);
+        }
+    }
+}
---
+++ Assets/Tutorials/CH1 Force Field/ForceFieldExample.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class ForceFieldExample : MonoBehaviour
+{
+    void Start() {
+        transform.DOPunchScale(new Vector3(0.2f, 0.2f, 0.2f),7,1).SetLoops(-1);    
+    }
+}
